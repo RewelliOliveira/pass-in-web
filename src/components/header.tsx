78f3acf97e4c0f4cc5b404b991b2f978d4fc8e29
RewlliOliveira <rewelli@alu.ufc.br>
2024-08-01 12:36:31 -0300

    terminando o front

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,11 @@
 import rewIcon from '../components/assets/rew-icon.svg'
 export function Header(){
     return(
-        <div>
-        <img src={rewIcon} />
-        <button className='bg-black'>OOOOOOOOOOOOOO</button>
-        <nav>
-            <a href="">Events</a>
-            <a href="">Participants</a>
+        <div className='flex items-center gap-5 py-2'>
+        <img src={rewIcon} className='h-8'/>
+        <nav className='flex items-center gap-5'>
+            <a href="" className='font-medium text-sm text-zinc-300'>Events</a>
+            <a href="" className='font-medium text-sm'>Participants</a>
         </nav>
         </div>
     )
